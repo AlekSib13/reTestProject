2af56e4d53227022986c91ba3993bc8e30f2fd75
Малинин Александр Юрьевич <malininayu@sibur.ru>
2022-10-24 23:50:42 +0300

    initial project set up

---
+++ reTestProject/reTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reTestProject
+//
+//  Created by Aleksandr Malinin on 24.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
