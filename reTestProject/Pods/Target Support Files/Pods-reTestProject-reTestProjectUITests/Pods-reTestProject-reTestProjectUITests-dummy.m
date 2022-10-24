680db7052c8b4433c8cd55982a3cf85a68bd93a9
Малинин Александр Юрьевич <malininayu@sibur.ru>
2022-10-24 23:54:36 +0300

    pods installed

---
+++ reTestProject/Pods/Target Support Files/Pods-reTestProject-reTestProjectUITests/Pods-reTestProject-reTestProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reTestProject_reTestProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_reTestProject_reTestProjectUITests
+@end
---
+++ reTestProject/Pods/Target Support Files/Pods-reTestProjectTests/Pods-reTestProjectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reTestProjectTests : NSObject
+@end
+@implementation PodsDummy_Pods_reTestProjectTests
+@end
