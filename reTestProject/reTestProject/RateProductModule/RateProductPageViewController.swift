31d43b3272415d1600a9f6f49a7ab924a9427cdc
Малинин Александр Юрьевич <malininayu@sibur.ru>
2022-10-25 22:54:16 +0300

    rate product model

--- reTestProject/reTestProject/RateProductModule/RateProductPageViewController.swift
+++ reTestProject/reTestProject/RateProductModule/RateProductPageViewController.swift
@@ -13,13 +13,23 @@ protocol RateProductPageViewControllerProtocol: AnyObject {}
 class RateProductPageViewController: UIPageViewController, RateProductPageViewControllerProtocol {
     
     let presenter: RateProductPresenterProtocol
+    var vc: RateProductViewControllerProtocol?
+    
     
     init(presenter: RateProductPresenterProtocol) {
         self.presenter = presenter
         super.init(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        
+        guard let window = UIApplication.shared.delegate?.window else {return}
+        print("the window is \(window)")
+        window?.rootViewController = self
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
 }
